ebfc6e8185077976214d89ad8e0fc337ccf37aab
Yanyi Bao <uoubyy@gmail.com>
2022-11-11 12:55:14 -0700

    add class to dialogue ui config

--- hrsaCCT/dialogue_ui_config/dialogue_ui_config.py
+++ hrsaCCT/dialogue_ui_config/dialogue_ui_config.py
@@ -2,8 +2,9 @@ import json
 import os.path
 
 import dearpygui.dearpygui as dpg
-
+import json
 import hrsa_cct_constants
+from configuration import hrsa_config
 
 
 # GUI Element Tags
@@ -34,70 +35,67 @@ def _callback_update_player_subtitle_text_color(sender, app_data, user_data):
     rgb_color = [int(value) for value in dpg.get_value(sender)[:-1]]
     rgb_color = tuple(rgb_color)
     hex_color = '#%02x%02x%02x' % rgb_color
-    print(rgb_color, hex_color)
+    # print(rgb_color, hex_color)
     global duc_color_setting
-    duc_color_setting['player']['subtitle_text_color'] = hex_color
+    duc_color_setting.player.ui.subtitle.text_color = hex_color
 
 def _callback_update_medicalstudent_subtitle_text_color(sender, app_data, user_data):
     rgb_color = [int(value) for value in dpg.get_value(sender)[:-1]]
     rgb_color = tuple(rgb_color)
     hex_color = '#%02x%02x%02x' % rgb_color
-    print(rgb_color, hex_color)
+    # print(rgb_color, hex_color)
     global duc_color_setting
-    duc_color_setting['medicalstudent']['subtitle_text_color'] = hex_color
+    duc_color_setting.medicalstudent.ui.subtitle.text_color = hex_color
 
 def _callback_update_patient_subtitle_text_color(sender, app_data, user_data):
     rgb_color = [int(value) for value in dpg.get_value(sender)[:-1]]
     rgb_color = tuple(rgb_color)
     hex_color = '#%02x%02x%02x' % rgb_color
-    print(rgb_color, hex_color)
+    # print(rgb_color, hex_color)
     global duc_color_setting
-    duc_color_setting['patient']['subtitle_text_color'] = hex_color
+    duc_color_setting.patient.ui.subtitle.text_color = hex_color
 
 def _callback_update_trainer_subtitle_text_color(sender, app_data, user_data):
     rgb_color = [int(value) for value in dpg.get_value(sender)[:-1]]
     rgb_color = tuple(rgb_color)
     hex_color = '#%02x%02x%02x' % rgb_color
-    print(rgb_color, hex_color)
+    # print(rgb_color, hex_color)
     global duc_color_setting
-    duc_color_setting['trainer']['subtitle_text_color'] = hex_color
+    duc_color_setting.trainer.ui.subtitle.text_color = hex_color
 
 
-duc_color_setting = {
-    "player": {
-        "subtitle_text_color": ""
-    },
-    "medicalstudent": {
-        "subtitle_text_color": ""
-    },
-    "patient": {
-        "subtitle_text_color": ""
-    },
-    "trainer": {
-        "subtitle_text_color": ""
-    }}
+duc_color_setting = None
 
-def _load_default_color_set():
+def _load_default_color_set(sender, app_data, user_data):
     global duc_color_setting
-    duc_color_setting['player']['subtitle_text_color'] = '#003f5c'
-    duc_color_setting['medicalstudent']['subtitle_text_color'] = '#7a5195'
-    duc_color_setting['patient']['subtitle_text_color'] = '#ef5675'
-    duc_color_setting['trainer']['subtitle_text_color'] = '#ffa600'
+    with open('dialogue_ui_config/dialogue_ui_config.json', "r", encoding="UTF-8") as ui_config_file:
+        row_data = ui_config_file.read()
+        row_config = json.loads(row_data)
+        duc_color_setting = hrsa_config.HRSAConfig(**row_config)
+
+
+def _save_color_set(sender, app_data, user_data):
+    global duc_color_setting
+    duc_color_setting_json = json.dumps(duc_color_setting.toJson(), indent=4)
+    with open('dialogue_ui_config/dialogue_ui_config_back.json', "w", encoding="UTF-8") as outfile:
+        outfile.write(duc_color_setting_json)
 
 def init_ui():
-    _load_default_color_set()
+    _load_default_color_set(None, None, None)
 
     with dpg.collapsing_header(label="Dialogue UI Config", default_open=True, parent=hrsa_cct_constants.HRSA_CCT_TOOL):
         # TODO: UI Creation
-        global duc_color_setting
-        dpg.add_color_edit(default_value=_hex_to_rgb(duc_color_setting['player']['subtitle_text_color']), label="Player Subtitle Text Color",
+        dpg.add_button(label="Load Setting", callback=_load_default_color_set)
+        dpg.add_button(label="Save Setting", callback=_save_color_set)
+
+        dpg.add_color_edit(default_value=_hex_to_rgb(duc_color_setting.player.ui.subtitle.text_color), label="Player Subtitle Text Color",
                            tag="DUC_PLAYER_SUBTITLE_TEXT_COLOR", callback=_callback_update_player_subtitle_text_color, input_mode=dpg.mvColorEdit_input_rgb)
 
-        dpg.add_color_edit(default_value=_hex_to_rgb(duc_color_setting['medicalstudent']['subtitle_text_color']), label="Medical Student Subtitle Text Color",
+        dpg.add_color_edit(default_value=_hex_to_rgb(duc_color_setting.medicalstudent.ui.subtitle.text_color), label="Medical Student Subtitle Text Color",
                            tag="DUC_MEDICALSTUDENT_SUBTITLE_TEXT_COLOR", callback=_callback_update_medicalstudent_subtitle_text_color, input_mode=dpg.mvColorEdit_input_rgb)
 
-        dpg.add_color_edit(default_value=_hex_to_rgb(duc_color_setting['patient']['subtitle_text_color']), label="Patient Subtitle Text Color",
+        dpg.add_color_edit(default_value=_hex_to_rgb(duc_color_setting.patient.ui.subtitle.text_color), label="Patient Subtitle Text Color",
                            tag="DUC_PATIENT_SUBTITLE_TEXT_COLOR", callback=_callback_update_patient_subtitle_text_color, input_mode=dpg.mvColorEdit_input_rgb)
 
-        dpg.add_color_edit(default_value=_hex_to_rgb(duc_color_setting['trainer']['subtitle_text_color']), label="Trainer Subtitle Text Color",
+        dpg.add_color_edit(default_value=_hex_to_rgb(duc_color_setting.trainer.ui.subtitle.text_color), label="Trainer Subtitle Text Color",
                            tag="DUC_TRAINER_SUBTITLE_TEXT_COLOR", callback=_callback_update_trainer_subtitle_text_color, input_mode=dpg.mvColorEdit_input_rgb)
