9467461ff44eed9605b820aa327698efc0a726b2
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2022-11-11 12:38:47 -0700

    Cache the patient information from the default language (en_US) folder for a newly created scenario

--- hrsaCCT/patient_info_ui/patient_info_ui.py
+++ hrsaCCT/patient_info_ui/patient_info_ui.py
@@ -4,6 +4,7 @@ import os.path
 import dearpygui.dearpygui as dpg
 
 import hrsa_cct_constants
+import hrsa_cct_globals
 
 # GUI Element Tags
 PIU_PATIENT_FIRST_NAME_INPUT_TEXT: str = "PIU_PATIENT_FIRST_NAME_INPUT_TEXT"
@@ -43,6 +44,7 @@ family_health_history_num = 0
 patient_info = dict()
 patient_info_saved = False
 piu_scenario_path = ""
+patient_information_json_file_path = ""
 
 problem_tab = None
 sdoh_problem_tab = None
@@ -52,10 +54,11 @@ allergy_tab = None
 
 
 def set_scenario_path(scenario_path):
-    global piu_scenario_path
+    global piu_scenario_path, patient_information_json_file_path
     piu_scenario_path = scenario_path
-    patient_information_json_file_path = os.path.join(piu_scenario_path, hrsa_cct_constants.PATIENT_INFORMATION_JSON_FILE_NAME)
+    patient_information_json_file_path = os.path.join(piu_scenario_path, hrsa_cct_globals.default_language_code, hrsa_cct_constants.PATIENT_INFORMATION_JSON_FILE_NAME)
     dpg.configure_item(PIU_SCENARIO_PATIENT_INFO_JSON_PATH_TEXT, default_value=patient_information_json_file_path)
+    _callback_load_patient_info_file(None, app_data=dict(file_path_name=patient_information_json_file_path))
 
 
 def generate_tag(key, sub_key, index=0):
@@ -185,9 +188,10 @@ def load_patient_info(key, sub_dict):
 
 
 def _callback_load_patient_info_file(sender, app_data):
-    global patient_info, piu_scenario_path
-    patient_info_json_file_path = app_data["file_path_name"]
-    with open(patient_info_json_file_path, "r", encoding="UTF-8") as patient_info_json:
+    global patient_info, piu_scenario_path, patient_information_json_file_path
+    patient_information_json_file_path = app_data["file_path_name"]
+    dpg.configure_item(PIU_SCENARIO_PATIENT_INFO_JSON_PATH_TEXT, default_value=str(patient_information_json_file_path))
+    with open(patient_information_json_file_path, "r", encoding="UTF-8") as patient_info_json:
         patient_info = json.load(patient_info_json)
 
         for key in patient_info:
@@ -439,7 +443,9 @@ def _callback_update_family_health_history(sender, app_data, user_data):
 
 
 def _callback_export_patient_info(sender, app_data, user_data):
-    dpg.show_item("PIU_SAVE_FILE_DIALOG")
+    global patient_information_json_file_path
+    # dpg.show_item("PIU_SAVE_FILE_DIALOG")
+    _callback_save_patient_into_to_file(None, app_data=dict(file_path_name=patient_information_json_file_path))
 
 
 def init_ui():
@@ -449,13 +455,13 @@ def init_ui():
         patient_info = json.load(patient_info_json)
 
     with dpg.collapsing_header(label="Patient Info UI", default_open=False, parent=hrsa_cct_constants.HRSA_CCT_TOOL):
-        # TODO: UI Creation
+        # TODO: Add a "Clear Data Button" that clears all the UI information
 
         dpg.add_text(tag=PIU_SCENARIO_PATIENT_INFO_JSON_PATH_TEXT)
 
-        dpg.file_dialog(height=300, width=600, directory_selector=True, show=False,
-                        callback=_callback_load_patient_info_file, tag="PIU_OPEN_FILE_DIALOG", modal=True)
-        # dpg.add_file_extension(".json", color=(255, 255, 0, 255))
+        with dpg.file_dialog(height=300, width=600, directory_selector=False, show=False,
+                             callback=_callback_load_patient_info_file, tag="PIU_OPEN_FILE_DIALOG", modal=True):
+            dpg.add_file_extension(".json", color=(255, 255, 0, 255))
 
         with dpg.file_dialog(height=300, width=600, directory_selector=False, show=False,
                              callback=_callback_save_patient_into_to_file, tag="PIU_SAVE_FILE_DIALOG", modal=True):
@@ -476,7 +482,7 @@ def init_ui():
             # create_new_patient_info = dpg.add_button(
             #     label="Create Patient Information", indent=20)
             load_exist_patient_info = dpg.add_button(
-                label="Select Scenario", callback=_callback_load_patient_info)
+                label="Select Patient Information JSON file", callback=_callback_load_patient_info)
         # with dpg.collapsing_header(label="Create Patient Information", default_open=False):
 
         with dpg.collapsing_header(label="Patient Demographics", default_open=True, indent=20):
