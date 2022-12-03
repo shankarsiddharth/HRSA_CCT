27b3cd9188da37415f223351cda37529c8663e6e
Yanyi Bao <uoubyy@gmail.com>
2022-12-03 00:14:31 -0700

    display character model image

--- hrsaCCT/character_config/character_config.py
+++ hrsaCCT/character_config/character_config.py
@@ -15,19 +15,26 @@ student_ethnicity_combo = None
 trainer_gender_combo = None
 trainer_ethnicity_combo = None
 
-def _get_characters_of_type(condictions):
+patient_model_window = None
+student_model_window = None
+trainer_model_window = None
+
+loaded_texture = []
+
+def _get_characters_of_type(conditions):
+    print('search conditions ', conditions)
     data_of_type = []
     global model_data_list
 
     for data in model_data_list:
         matched = True
-        for key in condictions:
+        for key in conditions:
             if not key in dir(data.metaData):
                 matched = False
                 break
 
             data_type = getattr(data.metaData, key)
-            if not data_type == condictions[key]:
+            if not data_type == conditions[key]:
                 matched = False
                 break
 
@@ -35,6 +42,7 @@ def _get_characters_of_type(condictions):
             data_of_type.append(data)
 
     return data_of_type
+
 
 def _load_character_config():
     global model_data_list
@@ -46,7 +54,7 @@ def _load_character_config():
             item = character_model_data.CharacterModelData(**data)
             model_data_list.append(item)
 
-        print(_get_characters_of_type({'CharacterType': 'kMedicalStudent'}))
+        # print(_get_characters_of_type({'CharacterType': 'kMedicalStudent'}))
 
 
 def _log(sender, app_data, user_data):
@@ -55,23 +63,57 @@ def _log(sender, app_data, user_data):
     # sender: 130, 	 app_data: White, 	 user_data: None
 
 
+def _load_character_model_image(image_name):
+    width, height, channels, data = dpg.load_image('assets/avatar/' + image_name + '.png')
+    dpg.add_static_texture(width=width, height=height, default_value=data, tag=image_name, parent='static_texture_container')
+    global loaded_texture
+    loaded_texture.insert(0, image_name)
+
+
 def _callback_update_filter(sender, app_data, user_data):
     global patient_gender_combo, patient_ethnicity_combo
+
     gender = dpg.get_value(patient_gender_combo)
     ethnicity = dpg.get_value(patient_ethnicity_combo)
-    condictions = {}
-    condictions['CharacterType'] = 'k' + user_data
-    if not gender == 'None':
-        condictions['GenderType'] = 'k' + gender
-    if not ethnicity == 'None':
-        condictions['EthnicityType'] = 'k' + ethnicity
 
-    patients_data = _get_characters_of_type(condictions)
-    for patient in patients_data:
-        print(patient.uid)
+    global patient_model_window, student_model_window, trainer_model_window
+    target_window = patient_model_window
+    if user_data == 'MedicalStudent':
+        target_window = student_model_window
+        gender = dpg.get_value(student_gender_combo)
+        ethnicity = dpg.get_value(student_ethnicity_combo)
+    elif user_data == 'Trainer':
+        target_window = trainer_model_window
+        gender = dpg.get_value(trainer_gender_combo)
+        ethnicity = dpg.get_value(trainer_ethnicity_combo)
+
+    conditions = {}
+
+    conditions['CharacterType'] = 'k' + user_data
+    if not gender == 'None':
+        conditions['GenderType'] = 'k' + gender
+    if not ethnicity == 'None':
+        conditions['EthnicityType'] = 'k' + ethnicity
+
+    # print(conditions)
+    patients_data = _get_characters_of_type(conditions)
+
+    dpg.delete_item(target_window, children_only=True)
+    global loaded_texture
+    with dpg.group(horizontal=True, indent=20, parent=target_window):
+        for patient in patients_data:
+            print(patient.uid)
+            if patient.uid not in loaded_texture:
+                # print('texture not loaded')
+                _load_character_model_image(patient.uid)
+            dpg.add_image_button(patient.uid, callback=_log, tag='uid_' + patient.uid)
+            # dpg.add_text(patient.uid, parent=target_window)
 
 def init_ui():
     _load_character_config()
+
+    dpg.add_texture_registry(label="Demo Texture Container", tag="static_texture_container")
+
     with dpg.collapsing_header(label="Character Config", default_open=True, parent=hrsa_cct_constants.HRSA_CCT_TOOL):
         # TODO: UI Creation
         dpg.add_text('Patient', indent=20)
@@ -80,12 +122,23 @@ def init_ui():
             patient_gender_combo = dpg.add_combo(('None', 'Male', 'Female'), label='Gender', default_value='None', callback=_callback_update_filter, width=200, user_data='Patient')
             patient_ethnicity_combo = dpg.add_combo(('None', 'White', 'Black', 'Hispanic'), label='Ethnicity', default_value='None', callback=_callback_update_filter, width=200, user_data='Patient')
 
+        global patient_model_window
+        patient_model_window = dpg.add_child_window(autosize_x=True, height=250, menubar=True)
+
+        dpg.add_text('Medical Student', indent=20)
         with dpg.group(horizontal=True, indent=20):
             global student_gender_combo, student_ethnicity_combo
             student_gender_combo = dpg.add_combo(('None', 'Male', 'Female'), label='Gender', default_value='None', callback=_callback_update_filter, width=200, user_data='MedicalStudent')
             student_ethnicity_combo = dpg.add_combo(('None', 'White', 'Black', 'Hispanic'), label='Ethnicity', default_value='None', callback=_callback_update_filter, width=200, user_data='MedicalStudent')
 
+        global student_model_window
+        student_model_window = dpg.add_child_window(autosize_x=True, height=250, menubar=True)
+
+        dpg.add_text('Trainer', indent=20)
         with dpg.group(horizontal=True, indent=20):
             global trainer_gender_combo, trainer_ethnicity_combo
             trainer_gender_combo = dpg.add_combo(('None', 'Male', 'Female'), label='Gender', default_value='None', callback=_callback_update_filter, width=200, user_data='Trainer')
             trainer_ethnicity_combo = dpg.add_combo(('None', 'White', 'Black', 'Hispanic'), label='Ethnicity', default_value='None', callback=_callback_update_filter, width=200, user_data='Trainer')
+
+        global trainer_model_window
+        trainer_model_window = dpg.add_child_window(autosize_x=True, height=250, menubar=True)
