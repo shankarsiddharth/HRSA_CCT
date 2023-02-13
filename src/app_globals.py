8d2af32219d3d9f24f5606381b073d2f04c95a06
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2023-02-12 19:28:51 -0700

    Create App Global and App Version

---
+++ src/app_globals.py
@@ -0,0 +1,7 @@
+from app_file_system_constants import AppFileSystemConstants
+from app_file_system import AppFileSystem
+from app_logger import AppLogger
+
+afsc: AppFileSystemConstants = AppFileSystemConstants()
+afs: AppFileSystem = AppFileSystem()
+log: AppLogger = AppLogger()
