20fb43da25ad65a77f8a0e92469ffd0f07e6e330
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2023-02-12 19:27:13 -0700

    Application Logger with File logger & UI logger

---
+++ src/app_logger.py
@@ -0,0 +1,213 @@
+import logging
+import os
+import pathlib
+import sys
+import threading
+import traceback
+from logging.handlers import SocketHandler
+
+import app_logging_custom as alc
+from app_file_system_constants import AppFileSystemConstants
+from app_file_system import AppFileSystem
+from app_logger_ui import AppUILogger
+
+
+class AppLogger(object):
+    _instance = None
+
+    _lock = threading.Lock()
+
+    def __new__(cls):
+        if cls._instance is None:
+            with cls._lock:
+                if cls._instance is None:
+                    print('Creating the Logger object')
+                    cls._instance = super(AppLogger, cls).__new__(cls)
+                    cls._instance.__initialize__()
+        return cls._instance
+
+    def __initialize__(self):
+        self.afsc = AppFileSystemConstants()
+        self.afs = AppFileSystem()
+        self.should_log_to_ui = True
+
+        print("Creating Logger Instance...")
+
+        print("Getting root folder...")
+        root_folder_path = self.afs.get_root_folder()
+
+        print("Checking log folder...")
+        self.log_folder_path = os.path.join(root_folder_path, self.afsc.LOG_FOLDER_NAME)
+        self.log_folder = pathlib.Path(self.log_folder_path)
+        if not self.log_folder.exists():
+            print("Log folder does not exist. Creating it.")
+            print("Creating log folder: " + self.log_folder_path)
+            try:
+                os.makedirs(self.log_folder_path)
+            except OSError as e:
+                traceback_string = traceback.format_exc()
+                exception_message = ("Creation of the log folder %s failed" % self.log_folder_path) + str(e) + "\n" + traceback_string
+                print(exception_message)
+
+        print("Log Folder exists.")
+        self.log_file_path = os.path.join(self.log_folder_path, self.afsc.LOG_FILE_NAME)
+
+        self.backup_log_file_path = os.path.join(self.log_folder_path, self.afsc.BACKUP_LOG_FILE_NAME)
+
+        self.log_file = pathlib.Path(self.log_file_path)
+        self.backup_log_file = pathlib.Path(self.backup_log_file_path)
+        maximum_log_file_size_in_megabytes = 10  # 10 MB
+        maximum_log_file_size_in_bytes = maximum_log_file_size_in_megabytes * 1024 * 1024  # MBs to bytes
+
+        self.logger_instance = logging.getLogger('HRSA CCT Log')
+        self.logger_instance.setLevel(1)  # to send all records to log
+
+        # Attach a console handler to the logger
+        console_handler = logging.StreamHandler(stream=sys.stdout)
+        # create formatter
+        console_formatter = logging.Formatter(fmt='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+        console_handler.setFormatter(console_formatter)
+        self.logger_instance.addHandler(console_handler)
+
+        # Attach SocketHandler - Visual Log Viewer - cutelog
+        socket_handler = SocketHandler('127.0.0.1', 19996)  # default listening address
+        self.logger_instance.addHandler(socket_handler)
+
+        # Handle Log files
+        log_file_write_mode = 'a'
+        if self.log_file.is_file():
+            # Log File Exists
+            current_log_file_size = os.path.getsize(self.log_file)
+            if current_log_file_size >= maximum_log_file_size_in_bytes:
+                if self.backup_log_file.is_file():
+                    # Backup Log file Exists
+                    os.remove(self.backup_log_file_path)
+                os.rename(self.log_file_path, self.backup_log_file_path)
+        else:
+            # Log File Does Not Exist
+            log_file_write_mode = 'w'
+            with open(self.log_file_path, log_file_write_mode) as log_file:
+                log_file.write('')
+
+        # Attach File Logger
+        file_handler = logging.FileHandler(self.log_file_path, mode=log_file_write_mode, encoding="UTF-8")
+        file_formatter = logging.Formatter(fmt='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+        file_handler.setFormatter(file_formatter)
+        self.logger_instance.addHandler(file_handler)
+
+        # Test Logs
+        # self.logger_instance.log(1, '======================================== HRSA CCT LOGGER STARTED ========================================')
+        self.logger_instance.debug('======================================== HRSA CCT LOGGER STARTED ========================================')
+        # self.logger_instance.debug('Test Debug')
+        # self.logger_instance.info('Test Info')
+        # self.logger_instance.log(c_logging.log_level_success, 'Test Success')
+        # self.logger_instance.warning('Test Warning')
+        # self.logger_instance.error('Test Error')
+        # self.logger_instance.critical('Test Critical')
+
+        self.ui_logger = None
+        self.log_level = 0
+        self.count = 0
+        # self.flush_count = 10000
+
+    def init_ui(self, parent=None):
+        self.ui_logger: AppUILogger = AppUILogger(parent=parent)
+        self.ui_logger.trace("HRSA CCT UI Logger Started")
+        if sys.flags.dev_mode:
+            self.ui_logger.debug('Test UI Debug')
+            self.ui_logger.info('Test UI Info')
+            self.ui_logger.success('Test UI Success')
+            self.ui_logger.warning('Test UI Warning')
+            self.ui_logger.error('Test UI Error')
+            self.ui_logger.critical('Test UI Critical')
+
+            self.debug('Test Debug')
+            self.info('Test Info')
+            self.success('Test Success')
+            self.warning('Test Warning')
+            self.error('Test Error')
+            self.critical('Test Critical')
+
+    def close_ui(self):
+        if self.ui_logger is not None:
+            self.ui_logger = None
+
+    def _log(self, level, message, *args):
+
+        if level < self.log_level:
+            return
+
+        self.count += 1
+
+        # if self.count > self.flush_count:
+        #     self.clear_log()
+
+        self.log_message = message
+        if args:
+            self.log_message = message % args
+
+        if logging.NOTSET <= level < logging.DEBUG:
+            self.logger_instance.log(1, message, *args)
+            if self.ui_logger is not None and self.should_log_to_ui:
+                self.ui_logger.trace(message, *args)
+
+        elif level == logging.DEBUG:
+            self.logger_instance.debug(message, *args)
+            if self.ui_logger is not None and self.should_log_to_ui:
+                self.ui_logger.debug(message, *args)
+
+        elif level == logging.INFO:
+            self.logger_instance.info(message, *args)
+            if self.ui_logger is not None and self.should_log_to_ui:
+                self.ui_logger.info(message, *args)
+
+        elif level == alc.log_level_success:
+            self.logger_instance.log(alc.log_level_success, message, *args)
+            if self.ui_logger is not None and self.should_log_to_ui:
+                self.ui_logger.success(message, *args)
+
+        elif level == logging.WARNING:
+            self.logger_instance.warning(message, *args)
+            if self.ui_logger is not None and self.should_log_to_ui:
+                self.ui_logger.warning(message, *args)
+
+        elif level == logging.ERROR:
+            self.logger_instance.error(message, *args)
+            if self.ui_logger is not None and self.should_log_to_ui:
+                self.ui_logger.error(message, *args)
+
+        elif level == logging.CRITICAL:
+            self.logger_instance.critical(message, *args)
+            if self.ui_logger is not None:
+                self.ui_logger.critical(message, *args)
+
+        # print(message)
+
+    def trace(self, message, *args: object):
+        self._log(logging.NOTSET, message, *args)
+
+    def debug(self, message, *args: object):
+        self._log(logging.DEBUG, message, *args)
+
+    def info(self, message, *args: object):
+        self._log(logging.INFO, message, *args)
+
+    def success(self, message, *args: object):
+        self._log(alc.log_level_success, message, *args)
+
+    def warning(self, message, *args: object):
+        self._log(logging.WARNING, message, *args)
+
+    def error(self, message, *args: object):
+        self._log(logging.ERROR, message, *args)
+
+    def critical(self, message, *args: object):
+        self._log(logging.CRITICAL, message, *args)
+
+    def exception(self, message, *args: object):
+        self.logger_instance.exception(message, *args)
+        if self.ui_logger is not None:
+            self.ui_logger.exception(message, *args)
+
+    def clear_log(self):
+        pass
---
+++ src/app_logger_ui.py
@@ -0,0 +1,167 @@
+import sys
+import logging
+
+import dearpygui.dearpygui as dpg
+
+import app_logging_custom as alc
+
+
+class AppUILogger:
+
+    def __init__(self, parent, is_open=True):
+
+        self.log_level = 0
+        self._auto_scroll = True
+        self._default_show_all_logs = False
+        if sys.flags.dev_mode:
+            self._default_show_all_logs = True
+        self._show_all_logs_tag = "UI Logger Show All Logs"
+        self._filter_text_tag = "UI Logger Filter Text Box"
+        self.filter_id = None
+        if parent:
+            self.window_id = parent
+            # self.window_id = dpg.add_collapsing_header(label="Logger", tag="Logger", default_open=is_open, parent=parent)
+        else:
+            self.window_id = dpg.add_window(label="HRSA CCT Logger", no_title_bar=False, no_close=True)
+            # self.window_id = dpg.add_collapsing_header(label="Logger", tag="Logger", default_open=is_open)
+        self.count = 0
+        self.flush_count = 10000
+
+        # with dpg.add_collapsing_header(label="Logger", default_open=True, parent=self.window_id):
+        with dpg.group(horizontal=True, parent=self.window_id):
+            dpg.add_checkbox(label="Auto-scroll", default_value=True, callback=lambda sender: self.auto_scroll(dpg.get_value(sender)))
+            dpg.add_checkbox(tag=self._show_all_logs_tag, label="Show All Logs", default_value=self._default_show_all_logs, callback=self.__callback_show_all_logs)
+            dpg.add_button(label="Clear", callback=lambda: dpg.delete_item(self.filter_id, children_only=True))
+
+        dpg.add_input_text(tag=self._filter_text_tag, label="Filter", callback=self.__callback_on_filter_text_changed,
+                           parent=self.window_id)
+        self.child_id = dpg.add_child_window(parent=self.window_id, autosize_x=True, autosize_y=True)
+        # self.child_id = dpg.add_child(parent=self.window_id, autosize_x=True, autosize_y=True)
+        self.filter_id = dpg.add_filter_set(parent=self.child_id)
+
+        with dpg.theme() as self.trace_theme:
+            with dpg.theme_component(0):
+                dpg.add_theme_color(dpg.mvThemeCol_Text, (255, 127, 80, 255))
+
+        with dpg.theme() as self.debug_theme:
+            with dpg.theme_component(0):
+                dpg.add_theme_color(dpg.mvThemeCol_Text, (64, 128, 255, 255))
+
+        with dpg.theme() as self.info_theme:
+            with dpg.theme_component(0):
+                dpg.add_theme_color(dpg.mvThemeCol_Text, (255, 255, 255, 255))
+
+        with dpg.theme() as self.success_theme:
+            with dpg.theme_component(0):
+                dpg.add_theme_color(dpg.mvThemeCol_Text, (0, 255, 0, 255))
+
+        with dpg.theme() as self.warning_theme:
+            with dpg.theme_component(0):
+                dpg.add_theme_color(dpg.mvThemeCol_Text, (255, 255, 0, 255))
+
+        with dpg.theme() as self.error_theme:
+            with dpg.theme_component(0):
+                dpg.add_theme_color(dpg.mvThemeCol_Text, (255, 0, 0, 255))
+
+        with dpg.theme() as self.critical_theme:
+            with dpg.theme_component(0):
+                dpg.add_theme_color(dpg.mvThemeCol_Text, (255, 0, 0, 255))
+
+        self.__on_show_all_logs()
+
+    def auto_scroll(self, value):
+        self._auto_scroll = value
+
+    def __callback_show_all_logs(self, sender):
+        self.__on_show_all_logs()
+
+    def __on_show_all_logs(self):
+        show_all_logs = dpg.get_value(self._show_all_logs_tag)
+        if show_all_logs:
+            dpg.set_value(self.filter_id, '')
+        else:
+            dpg.set_value(self.filter_id, 'INFO, SUCCESS, WARNING, ERROR, CRITICAL')
+
+    def __callback_on_filter_text_changed(self, sender):
+        filter_text = dpg.get_value(self._filter_text_tag)
+        if str(filter_text) != '':
+            dpg.configure_item(self._show_all_logs_tag, default_value=True)
+            dpg.set_value(self.filter_id, filter_text)
+        else:
+            dpg.configure_item(self._show_all_logs_tag, default_value=False)
+            self.__on_show_all_logs()
+
+    def _log(self, level, message, *args):
+
+        if level < self.log_level:
+            return
+
+        self.count += 1
+
+        if self.count > self.flush_count:
+            self.clear_log()
+
+        theme = self.info_theme
+        self.ui_message = message
+        self.log_message = message
+        if args:
+            self.log_message = message % args
+
+        if level == logging.NOTSET:
+            self.ui_message = "[TRACE]\t\t" + self.log_message
+            theme = self.trace_theme
+        elif level == logging.DEBUG:
+            self.ui_message = "[DEBUG]\t\t" + self.log_message
+            theme = self.debug_theme
+        elif level == logging.INFO:
+            self.ui_message = "[INFO]\t\t" + self.log_message
+            theme = self.info_theme
+        elif level == alc.log_level_success:
+            self.ui_message = "[SUCCESS]\t\t" + self.log_message
+            theme = self.success_theme
+        elif level == logging.WARNING:
+            self.ui_message = "[WARNING]\t\t" + self.log_message
+            theme = self.warning_theme
+        elif level == logging.ERROR:
+            self.ui_message = "[ERROR]\t\t" + self.log_message
+            theme = self.error_theme
+        elif level == logging.CRITICAL:
+            self.ui_message = "[CRITICAL]\t\t" + self.log_message
+            theme = self.critical_theme
+        elif level == alc.log_level_exception:
+            self.ui_message = "[EXCEPTION]\t\t" + self.log_message
+            theme = self.critical_theme
+
+        new_log = dpg.add_text(self.ui_message, parent=self.filter_id, filter_key=self.ui_message)
+        dpg.bind_item_theme(new_log, theme)
+        if self._auto_scroll:
+            scroll_max = dpg.get_y_scroll_max(self.child_id)
+            dpg.set_y_scroll(self.child_id, -1.0)
+
+    def trace(self, message, *args):
+        self._log(logging.NOTSET, message, *args)
+
+    def debug(self, message, *args):
+        self._log(logging.DEBUG, message, *args)
+
+    def info(self, message, *args):
+        self._log(logging.INFO, message, *args)
+
+    def success(self, message, *args):
+        self._log(alc.log_level_success, message, *args)
+
+    def warning(self, message, *args):
+        self._log(logging.WARNING, message, *args)
+
+    def error(self, message, *args):
+        self._log(logging.ERROR, message, *args)
+
+    def critical(self, message, *args):
+        self._log(logging.CRITICAL, message, *args)
+
+    def exception(self, message, *args):
+        self._log(alc.log_level_exception, message, *args)
+
+    def clear_log(self):
+        dpg.delete_item(self.filter_id, children_only=True)
+        self.count = 0
