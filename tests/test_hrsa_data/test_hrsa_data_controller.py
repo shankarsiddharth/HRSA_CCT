ee35291b06c38ce6d972e2a6a695a5c83544d21e
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2023-02-28 23:59:08 -0700

    Add unit tests for hrsa data

---
+++ tests/test_hrsa_data/test_hrsa_data_controller.py
@@ -0,0 +1,52 @@
+import os.path
+import shutil
+import unittest
+
+from app_file_system.app_file_system import AppFileSystem
+from hrsa_data.hrsa_data_controller import HRSADataController
+
+
+class TestHRSADataController(unittest.TestCase):
+
+    def setUp(self):
+        self.hdc: HRSADataController = HRSADataController()
+        self.afs: AppFileSystem = AppFileSystem()
+
+    def delete_scenario_if_exist(self, test_scenario, ws_root):
+        test_scenario_path = os.path.join(ws_root, test_scenario)
+        if os.path.exists(test_scenario_path):
+            shutil.rmtree(test_scenario_path)
+            print("Deleted scenario path: " + test_scenario_path)
+        return test_scenario_path
+
+    def test_create_new_scenario_data(self):
+        # Workspace Root Path
+        ws_root = self.afs.get_user_hrsa_data_workspace_path()
+        # Scenario Path
+        test_scenario_1 = 'test_scenario_1'
+        test_scenario_1_path = self.delete_scenario_if_exist(test_scenario_1, ws_root)
+        print("Trying to create scenario path: " + test_scenario_1_path)
+        self.assertEqual(self.hdc.create_new_scenario_data(test_scenario_1), True)
+
+        test_scenario_2 = 'test_scenario_2'
+        test_scenario_2_path = self.delete_scenario_if_exist(test_scenario_2, ws_root)
+        print("Trying to create scenario path: " + test_scenario_2_path)
+        self.assertEqual(self.hdc.create_new_scenario_data(test_scenario_2), True)
+
+    def test_create_new_scenario_data_for_language(self):
+        # Workspace Root Path
+        ws_root = self.afs.get_user_hrsa_data_workspace_path()
+        # Scenario Path
+        test_scenario_1 = 'test_scenario_1'
+        self.hdc.delete_scenario_data(test_scenario_1)
+        print("Trying to create scenario path: " + test_scenario_1)
+        self.assertEqual(self.hdc.create_new_scenario_data(test_scenario_1, self.hdc.__hdfsc__.DEFAULT_LANGUAGE_CODE), True)
+
+        test_scenario_2 = 'test_scenario_2'
+        self.hdc.delete_scenario_data(test_scenario_2)
+        print("Trying to create scenario path: " + test_scenario_2)
+        self.assertEqual(self.hdc.create_new_scenario_data(test_scenario_2, self.hdc.__hdfsc__.DEFAULT_LANGUAGE_CODE), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_hrsa_data/test_scenario_data/test_scenario_config/test_scenario_config.py
@@ -0,0 +1,41 @@
+import json
+import os
+import unittest
+from dataclasses import asdict
+from types import SimpleNamespace
+
+from hrsa_data.scenario_data.scenario_config.scenario_config import ScenarioConfig
+from tests.helper import get_test_path_of_file
+
+
+class TestScenarioConfig(unittest.TestCase):
+    file_path_to_test = os.path.join(get_test_path_of_file(__file__), 'scenario_config_test.json')
+    file_path_to_create = os.path.join(get_test_path_of_file(__file__), 'new_scenario_config.json')
+
+    def test_load_from_string(self):
+        with open(self.file_path_to_test) as f:
+            text = f.read()
+            # Parse JSON into an object with attributes corresponding to dict keys.
+            hrsa_config_data: ScenarioConfig = json.loads(text, object_hook=lambda d: SimpleNamespace(**d))
+            self.assertEqual(hrsa_config_data.player_config.ui_config.subtitle_config.text_color, '#FFFFFF')
+            self.assertEqual(hrsa_config_data.conversation_config.question_timer_in_seconds, 120)
+
+    def test_load_from_dict(self):
+        with open(self.file_path_to_test) as f:
+            hrsa_config_data: ScenarioConfig = ScenarioConfig.from_dict(json.load(f))
+            self.assertEqual(hrsa_config_data.player_config.ui_config.subtitle_config.text_color, '#FFFFFF')
+            self.assertEqual(hrsa_config_data.conversation_config.question_timer_in_seconds, 120)
+
+    def test_create_object(self):
+        hrsa_config_data = ScenarioConfig()
+        self.assertEqual(hrsa_config_data.player_config.ui_config.subtitle_config.text_color, '#FFFFFF')
+        self.assertEqual(hrsa_config_data.conversation_config.question_timer_in_seconds, 0)
+
+    def test_create_new_file(self):
+        config_data: ScenarioConfig = ScenarioConfig()
+        with open(self.file_path_to_create, 'w') as f:
+            json.dump(asdict(config_data), f, indent=4)
+
+
+if __name__ == '__main__':
+    unittest.main()
