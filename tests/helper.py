b459d1739111ae824dc73e52f44c5d00ab29af28
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2023-03-04 19:26:17 -0700

    Add unit tests to check the caching of google cloud voice list

--- tests/helper.py
+++ tests/helper.py
@@ -1,5 +1,17 @@
+import os
 import pathlib
+import shutil
 
 
 def get_test_path_of_file(in_file):
     return pathlib.Path(in_file).parent.resolve()
+
+
+def copy_without_permission_bits(src, dst):
+    if os.path.isdir(dst):
+        dst = os.path.join(dst, os.path.basename(src))
+    shutil.copyfile(src, dst)
+
+
+def copy_file(src, dst):
+    shutil.copy(src, dst)
---
+++ tests/test_text_to_speech/test_google_cloud_tts.py
@@ -0,0 +1,54 @@
+import json
+import os.path
+import unittest
+from dataclasses import asdict
+from types import SimpleNamespace
+
+from tests.helper import get_test_path_of_file, copy_file
+from text_to_speech.google_cloud_tts import GoogleCloudTTS
+from text_to_speech.google_cloud_voice_data import GoogleCloudVoiceData
+
+
+class TestGoogleCloudTTS(unittest.TestCase):
+    file_to_create = os.path.join(get_test_path_of_file(__file__), 'new_google_cloud_voice_data.json')
+    file_to_test = os.path.join(get_test_path_of_file(__file__), 'google_cloud_voice_data_test.json')
+
+    def setUp(self):
+        self.gctts: GoogleCloudTTS = GoogleCloudTTS()
+
+    def process_google_cloud_voice_data(self, google_cloud_voice_data: GoogleCloudVoiceData):
+        # voice_data_dict = google_cloud_voice_data.voice_data.__dict__
+        voice_data_dict = vars(google_cloud_voice_data.voice_data)
+        if 'en-US' in voice_data_dict:
+            self.assertEqual(voice_data_dict['en-US'].language_code, 'en-US')
+        else:
+            self.fail('en-US not in google_cloud_voice_data.voice_data')
+
+        if 'es-US' in voice_data_dict:
+            self.assertEqual(voice_data_dict['es-US'].language_code, 'es-US')
+        else:
+            self.fail('es-US not in google_cloud_voice_data.voice_data')
+
+        if 'es-ES' in voice_data_dict:
+            self.assertEqual(voice_data_dict['es-ES'].language_code, 'es-ES')
+        else:
+            self.fail('es-ES not in google_cloud_voice_data.voice_data')
+
+    def test_create_new_file(self):
+        self.gctts.cache_voice_data()
+        google_cloud_voice_data: GoogleCloudVoiceData = self.gctts.get_voice_data()
+        with open(self.file_to_create, 'w') as f:
+            json.dump(asdict(google_cloud_voice_data), f, indent=4)
+
+        copy_file(self.file_to_create, self.file_to_test)
+
+    def test_load_from_string(self):
+        with open(self.file_to_test) as f:
+            text = f.read()
+            # Parse JSON into an object with attributes corresponding to dict keys.
+            google_cloud_voice_data: GoogleCloudVoiceData = json.loads(text, object_hook=lambda d: SimpleNamespace(**d))
+            self.process_google_cloud_voice_data(google_cloud_voice_data)
+
+
+if __name__ == '__main__':
+    unittest.main()
